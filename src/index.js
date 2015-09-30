c21d7a3063b575a47e3038adbcd4908a2eec01ad
Thomas <iammotivated@gmail.com>
2015-09-30 18:37:27 +0100

    Updated to correctly accept config
    When creating middleware, must be invoked with optional config object.

--- src/index.js
+++ src/index.js
@@ -2,42 +2,36 @@ import isPromise from './isPromise';
 
 const defaultTypes = ['PENDING', 'FULFILLED', 'REJECTED']
 
-export default function promiseMiddleware(config) {
+export default function promiseMiddleware(config={}) {
   const promiseTypes = config.promiseTypes || defaultTypes;
-  return next => action => {
-    if (!isPromise(action.payload)) {
-      return next(action);
-    }
+  return () => {
+    return next => action => {
+      if (!isPromise(action.payload)) {
+        return next(action);
+      }
 
-    const { type, payload, meta } = action;
-    const { promise, data } = payload;
-    const [ PENDING, FULFILLED, REJECTED ] = meta.promiseTypes || promiseTypes
+      const { type, payload, meta={} } = action;
+      const { promise, data } = payload;
+      const [ PENDING, FULFILLED, REJECTED ] = meta.promiseTypes || promiseTypes;
 
-    /**
-     * Dispatch the first async handler. This tells the
-     * reducer that an async action has been dispatched.
-     */
-    next({
-      type: `${type}_${PENDING}`,
-      payload: data,
-      meta
-    });
+      next({
+        type: `${type}_${PENDING}`,
+        payload: data,
+        meta
+      });
 
-    /**
-     * Return either the fulfilled action object or the rejected
-     * action object.
-     */
-    return promise.then(
-      payload => next({
-        payload,
-        meta,
-        type: `${type}_${FULFILLED}`,
-      }),
-      error => next({
-        payload: error,
-        error: true,
-        type: `${type}_${REJECTED}`,
-      })
-    );
-  };
+      return promise.then(
+        payload => next({
+          payload,
+          meta,
+          type: `${type}_${FULFILLED}`,
+        }),
+        error => next({
+          payload: error,
+          error: true,
+          type: `${type}_${REJECTED}`,
+        })
+      );
+    };
+  }
 }
