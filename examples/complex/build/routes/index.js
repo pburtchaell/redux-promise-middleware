efe757b2ef6791e1e895c423a3e99b454543c192
Patrick Burtchaell <patrick@pburtchaell.com>
2016-03-26 00:19:54 -0500

    Delete build folder

--- examples/complex/build/routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.handleGet = handleGet;
-function handleGet(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
-}
