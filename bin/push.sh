68425e0dd33be8126471aae370ba53d2befc0504
Patrick <patrick@pburtchaell.com>
2018-02-24 11:13:40 -0600

    Update script

--- bin/push.sh
+++ bin/push.sh
@@ -3,25 +3,24 @@ fetchContributors() {
 }
 
 setupGit() {
-  echo "Adding Patrick Burtchaell as a Git user..."
+  echo "Adding Travis CI as the Git user..."
   git config --global user.email "builds@travis-ci.org"
   git config --global user.name "Travis CI"
+
+  echo "Adding Git remote..."
+  git remote add origin-travis https://${GITHUB_TOKEN}@github.com/pburtchaell/redux-promise-middleware.git > /dev/null 2>&1
 }
 
 commitFiles() {
   # Use Travis default environment variables to get the branch name
   # See more: https://docs.travis-ci.com/user/environment-variables/
-  git checkout master
-  git branch -d ${TRAVIS_PULL_REQUEST_BRANCH}
-  git checkout -b ${TRAVIS_PULL_REQUEST_BRANCH}
+  git checkout ${TRAVIS_PULL_REQUEST_BRANCH}
   git add CONTRIBUTORS.md
   git commit --message "Update CONTRIBUTORS.md" --no-verify > /dev/null 2>&1
 }
 
 uploadFiles() {
   echo "Uploading commit to GitHub..."
-  git remote add origin-travis https://${GITHUB_TOKEN}@github.com/pburtchaell/redux-promise-middleware.git > /dev/null 2>&1
-
   git push --quiet --force origin-travis ${TRAVIS_PULL_REQUEST_BRANCH}
 }
 
