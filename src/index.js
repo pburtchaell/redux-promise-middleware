0df65373e1ee4c207c7a19e3cba22cdca8f297b6
Xuefei Li <frankvsense@gmail.com>
2018-04-18 14:05:15 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import isPromise from './isPromise.js';
 
 /**
- * Note to contributors: Please also remember to check and make sure 
- * that `index.d.ts` is also up to date with the implementation when 
+ * Note to contributors: Please also remember to check and make sure
+ * that `index.d.ts` is also up to date with the implementation when
  * you add new features or modify existing ones.
  */
 
