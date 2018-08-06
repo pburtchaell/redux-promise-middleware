1b74ebdf01041639e5b16ba07a12d5e2c4993ff5
zhanyuzhang <1130142064@qq.com>
2018-08-06 15:20:28 +0800

    update example

--- examples/with-default-config/webpack.config.js
+++ examples/with-default-config/webpack.config.js
@@ -1,8 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlwebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
   entry: './index.js',
   output: {
     path: __dirname,
     filename: 'dist.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({ template: path.resolve(__dirname, 'index.html') })
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true
   }
 };
